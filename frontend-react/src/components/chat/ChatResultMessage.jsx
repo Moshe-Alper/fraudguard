c01df2dae04e476774c6b026962f16cb3b24c6bd
Moshik Alper <moshikalper@gmail.com>
2026-06-13 18:17:06 +0300

    Remove ResultCard component from ChatResultMessage for cleaner output

--- frontend-react/src/components/chat/ChatResultMessage.jsx
+++ frontend-react/src/components/chat/ChatResultMessage.jsx
@@ -1,10 +1,8 @@
-import ResultCard from '../ResultCard'
 import BonusAlerts from '../BonusAlerts'
 
 export default function ChatResultMessage({ resultData, bonusData }) {
   return (
     <div>
-      <ResultCard result={resultData} onReset={null} />
       {bonusData?.length > 0 && <BonusAlerts bonuses={bonusData} />}
     </div>
   )
