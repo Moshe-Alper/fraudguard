836a2fe7476f1e7ce4fa9090eb209e9f5d58fb13
Moshik Alper <moshikalper@gmail.com>
2026-06-13 18:23:57 +0300

    Simplify ChatResultMessage component by removing unnecessary div wrapper

--- frontend-react/src/components/chat/ChatResultMessage.jsx
+++ frontend-react/src/components/chat/ChatResultMessage.jsx
@@ -1,9 +1,6 @@
 import BonusAlerts from '../BonusAlerts'
 
 export default function ChatResultMessage({ resultData, bonusData }) {
-  return (
-    <div>
-      {bonusData?.length > 0 && <BonusAlerts bonuses={bonusData} />}
-    </div>
-  )
+  if (!bonusData?.length) return null
+  return <BonusAlerts bonuses={bonusData} />
 }
